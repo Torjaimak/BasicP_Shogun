061692b9eb939e292deb8cf356318d32167485a9
Student <Student@Train11-3-PC209.control.ad.sit.kmutt.ac.th>
2025-07-22 16:15:57 +0700

    BasicP

---
+++ basicP_class2.py
@@ -0,0 +1,35 @@
+# x = 10
+# print(x)
+# num = 40
+# print(num)
+# print(type(num))
+# print(str(num))
+# print(type(str(num)))
+
+# num1 = int(input("num : "))
+# num2 = int(input("num : "))
+# print ("wow = ",num1)
+# print ("wow = ",num2)
+# print(num1>num2)
+
+# x = "yes"
+# y = "no"
+# if x == y :
+#     print("inside if : ", " x= = y")
+# print("outside if :","starterpack wow" )
+
+# A = int(input("ให้ค่าA :"))
+# B = int(input("ให้ค่าB :"))
+# if A == B :
+#     print("เสมอ")
+# elif A > B :
+#     print("A ชนะ ",A-B,"แต้ม")
+# elif B > A :
+#     print("B ชนะ ",B-A,"แต้ม")
+# else :
+#     print("wow")
+    
+    
+A = int(input"ใส่ระทางมา : ")
+if a 
+
